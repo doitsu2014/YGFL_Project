5779f1d55213373d6d8974522b45eaafd807ad1a
Duc Tran Huu <duc.tran@wisky.vn>
2018-04-15 10:57:09 +0700

    [YGFL] Apply Inspinia Templates

---
+++ Projects/YGFL/YGFL/Areas/Admin/Controllers/AlbumManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace YGFL.Areas.Admin.Controllers
+{
+    public class AlbumManagementController : Controller
+    {
+        // GET: Admin/AlbumManagement
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
