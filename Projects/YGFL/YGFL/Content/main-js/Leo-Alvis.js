f8f8805fe0bfdc5b73f32a65a1c89c7d65af3e97
Duc Tran Huu <duc.tran@wisky.vn>
2018-04-15 21:34:59 +0700

    [YGFL] Function Detect FOrm Data

--- Projects/YGFL/YGFL/Content/main-js/Leo-Alvis.js
+++ Projects/YGFL/YGFL/Content/main-js/Leo-Alvis.js
@@ -1,20 +1,22 @@
 ﻿'use-strict'
 
-var Utils = {
-
-    GetFieldsFromFormData: function (formData) {
-        var result = {};
+var Utils = function () {
+    this.GetFieldsFromFormData = function (formData, result) {
         for (var i = 0; i < formData.children.length; ++i) {
             try {
                 var child = formData.children[i];
                 if (child.localName === 'input') {
                     result[child.name] = child.value;
                 }
+
+                if (child.children && child.children.length) {
+                    this.GetFieldsFromFormData(child, result);
+                }
+
             } catch (e) {
                 console.log(e);
             }
         }
         return result;
     }
-
 }
